5578f7ec80e418a976d1593b3be7ff697afd2557
Michał Piwowarski <s27476@pjwstk.edu.pl>
2024-03-08 09:38:55 +0100

    test

--- APBD_02/Program.cs
+++ APBD_02/Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+
+static double GetAVG(int[] tab)
+{
+    int sum = 0;
+    for (int i = 0; i < tab.Length; i++)
+    {
+        sum += tab[i];
+    }
+
+    return sum / tab.Length;
+}
+
+int[] t = { 1, 2, 3, 4, 5 };
+Console.WriteLine(GetAVG(t));
